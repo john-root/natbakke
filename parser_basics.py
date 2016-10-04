57387fbb232ef1be69c01fc9c3f195147fb98665
Matt McGrattan <matt.mcgrattan@digirati.com>
2016-10-04 16:07:21 +0100

    Creates annos

--- parser_basics.py
+++ parser_basics.py
@@ -3,7 +3,36 @@
 import spacy.en
 
 
-def ocr_to_annos(ocr_text, word_index, word_list):
+def create_anno(text, xywh, target, entity, manifest_uri=None):
+    '''
+    '''
+    anno_dict = {}
+    anno_dict['@context'] = 'http://www.w3.org/ns/anno.jsonld'
+    anno_dict['@type'] = 'Annotation'
+    anno_dict['motivation'] = ['tagging', 'commenting']
+    comment_dict = {}
+    comment_dict['type'] = "TextualBody"
+    comment_dict['format'] = 'text/plain'
+    comment_dict['value'] = text
+    tag_dict = {}
+    tag_dict['purpose'] = "tagging"
+    tag_dict['value'] = 'entity:' + entity
+    anno_dict['body'] = [comment_dict, tag_dict]
+    selector_dict = {}
+    selector_dict['@type'] = 'FragmentSelector'
+    selector_dict['conformsTo'] = "http://www.w3.org/TR/media-frags/"
+    selector_dict['value'] = 'xywh=' + xywh
+    target_dict = {}
+    target_dict['selector'] = selector_dict
+    target_dict['source'] = target
+    target_dict['type'] = 'SpecificResource'
+    if manifest_uri:
+        target_dict['dcterms:isPartOf'] = manifest_uri
+    anno_dict['target'] = target_dict
+    return anno_dict
+
+
+def ocr_to_annos(ocr_text, word_index, word_list, canvas_id, manifest_id=None):
     parser = spacy.en.English()
     parsed = parser(unicode(ocr_text))
     lookup = [x.idx for x in parsed]
@@ -16,4 +45,11 @@ def ocr_to_annos(ocr_text, word_index, word_list):
         for p in range(int(entity.start), int(entity.end)):
             details = token_index[lookup[p]]
             print entity.text_with_ws
-            print details
+            print details[0][0]['xywh']
+            anno = create_anno(entity.text_with_ws.encode('utf-8'),
+                               details[0][0]['xywh'],
+                               canvas_id,
+                               entity.label_,
+                               manifest_id
+                               )
+            print anno
