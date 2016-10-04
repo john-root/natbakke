4a7feda64e5df374dd8400336fe2cede3283063f
Matt McGrattan <matt.mcgrattan@digirati.com>
2016-10-04 16:10:18 +0100

    Should return resource list.

--- parser_basics.py
+++ parser_basics.py
@@ -37,6 +37,7 @@ def ocr_to_annos(ocr_text, word_index, word_list, canvas_id, manifest_id=None):
     parsed = parser(unicode(ocr_text))
     lookup = [x.idx for x in parsed]
     token_index = {}
+    resource_list = []
     for token in parsed:
         token_index[token.idx] = [
             [a for a in word_list if a['id'] == x.keys()[0]]
@@ -44,12 +45,10 @@ def ocr_to_annos(ocr_text, word_index, word_list, canvas_id, manifest_id=None):
     for entity in parsed.ents:
         for p in range(int(entity.start), int(entity.end)):
             details = token_index[lookup[p]]
-            print entity.text_with_ws
-            print details[0][0]['xywh']
-            anno = create_anno(entity.text_with_ws.encode('utf-8'),
-                               details[0][0]['xywh'],
-                               canvas_id,
-                               entity.label_,
-                               manifest_id
-                               )
-            print anno
+            resource_list.append(create_anno(entity.text_with_ws.encode('utf-8'),
+                                             details[0][0]['xywh'],
+                                             canvas_id,
+                                             entity.label_,
+                                             manifest_id
+                                             ))
+    return resource_list
