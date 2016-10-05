80766b366ab01e6fdd63c6948d5949dceb78ea6f
Matt McGrattan <matt.mcgrattan@digirati.com>
2016-10-05 12:20:32 +0100

    made explicit that this is enqueueing canvases

--- queue_processor.py
+++ queue_processor.py
@@ -3,7 +3,7 @@ import canvas_processor
 import json
 from boto.sqs.message import RawMessage
 
-def enqueue(queue, manifest_uri):
+def canvases_enqueue(queue, manifest_uri):
     item = canvas_processor.Manifest(manifest_uri)
     for canvas in item.canvases:
         msg = {}
@@ -17,7 +17,7 @@ def enqueue(queue, manifest_uri):
 def main():
     conn = boto.sqs.connect_to_region("us-west-2")
     queue = conn.get_queue('ocr1')
-    enqueue(queue, manifest_uri='https://tomcrane.github.io/scratch/manifests/ida/m1011-santa-fe-1910-30.json')
+    canvases_enqueue(queue, manifest_uri='https://tomcrane.github.io/scratch/manifests/ida/m1011-santa-fe-1910-30.json')
 
 
 
