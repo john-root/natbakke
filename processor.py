d6590c170cd157dfdc162cc1f2020d8b2ca29a6a
Matt McGrattan <matt.mcgrattan@digirati.com>
2016-10-05 10:02:21 +0100

    Added crude count for progress monitoring.

--- processor.py
+++ processor.py
@@ -190,12 +190,17 @@ def main():
     # item = Manifest(
     #     uri='http://wellcomelibrary.org/iiif/b20086362/manifest')
     # print item.requested.uri
-    item = Manifest(
-        uri='http://tomcrane.github.io/scratch/manifests/ida/m1011-san-juan-1920-22.json')
+    # https://tomcrane.github.io/scratch/manifests/ida/m1011-santa-fe-1910-30.json
+    # item = Manifest(uri='http://tomcrane.github.io/scratch/manifests/ida/m1011-san-juan-1920-22.json')
+    item = Manifest(uri='https://tomcrane.github.io/scratch/manifests/ida/m1011-santa-fe-1910-30.json')
     # canvas = item.canvases[10]
+    total = len(item.canvases)
+    count = 1
     for canvas in item.canvases:
+        print 'Processing canvas %s of %s' % (count, total)
         processed = CanvasProcess(
             canvas_obj=canvas, manifest_id=item.requested.uri)
+        count +=1
 
 if __name__ == '__main__':
     main()
