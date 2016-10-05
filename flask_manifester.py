d1f23c50fd43728fa8fc08189173ee5c591c22b1
Matt McGrattan <matt.mcgrattan@digirati.com>
2016-10-05 13:28:56 +0100

    Quick test of flask routing.

---
+++ flask_manifester.py
@@ -0,0 +1,27 @@
+import flask
+from flask import request
+import boto.sqs
+import json
+from boto.sqs.message import RawMessage
+
+
+def manifest_enqueue():
+    conn = boto.sqs.connect_to_region("us-west-2")
+    manifest_queue = conn.get_queue('ocr0')
+    msg = {}
+    msg['manifest'] = 'https://tomcrane.github.io/scratch/manifests/ida/m1011-santa-fe-1910-30.json'
+    m = RawMessage()
+    m.set_body(json.dumps(msg, indent=4))
+    manifest_queue.write(m)
+
+
+def main():
+    app = flask.Flask(__name__)
+    @app.route('/queue_manifest', methods=['POST'])
+    def manifestor():
+    	manifest = request.args.get('manifest')
+    	print 'manifest'
+
+
+if __name__ == '__main__':
+    main()
