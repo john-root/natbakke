31d62f2e49af0c467c47a28e864131ab5dd91238
Matt McGrattan <matt.mcgrattan@digirati.com>
2016-10-05 11:41:47 +0100

    Test of queue write

--- queue_processor.py
+++ queue_processor.py
@@ -1,6 +1,7 @@
 import boto.sqs
 import canvas_processor
 import json
+from boto.sqs.message import RawMessage
 
 
 def main():
@@ -8,11 +9,13 @@ def main():
     queue = conn.get_queue('ocr1')
     item = canvas_processor.Manifest(
         uri='https://tomcrane.github.io/scratch/manifests/ida/m1011-santa-fe-1910-30.json')
-    for canvas in item.canvases:
-        msg = {}
-        msg['manifest'] = item.requested.uri
-        msg['canvas'] = canvas
-        print json.dumps(msg, indent=4)
+    # for canvas in item.canvases:
+    msg = {}
+    msg['manifest'] = item.requested.uri
+    msg['canvas'] = item.canvases[0] # canvas
+    m = RawMessage()
+    m.set_body(json.dumps(msg, indent=4))
+    status = queue.write(m)
 
 
 
