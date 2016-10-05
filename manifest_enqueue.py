a958abda8ee3248235c6bd499457b81580a0a400
Matt McGrattan <matt.mcgrattan@digirati.com>
2016-10-05 13:00:54 +0100

    Test enqueue of a single manifest

---
+++ manifest_enqueue.py
@@ -0,0 +1,18 @@
+import boto.sqs
+import canvas_processor
+import json
+from boto.sqs.message import RawMessage
+
+
+def main():
+    conn = boto.sqs.connect_to_region("us-west-2")
+    manifest_queue = conn.get_queue('ocr0')
+    msg = {}
+    msg['manifest'] = 'https://tomcrane.github.io/scratch/manifests/ida/m1011-santa-fe-1910-30.json'
+    m = RawMessage()
+    m.set_body(json.dumps(msg, indent=4))
+    queue.write(m)
+
+
+if __name__ == '__main__':
+    main()
