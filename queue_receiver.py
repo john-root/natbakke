822f85415f4e0cf206ce740ede4e13110a02204c
Matt McGrattan <matt.mcgrattan@digirati.com>
2016-10-05 12:03:59 +0100

    Switched to long polling

--- queue_receiver.py
+++ queue_receiver.py
@@ -6,17 +6,15 @@ import json
 def main():
     conn = boto.sqs.connect_to_region("us-west-2")
     queue = conn.get_queue('ocr1')
-    results = queue.get_messages()
-    print len(results)
-    for result in results:
-        job = json.loads(result.get_body())
-        canvas = job['canvas']
-        manifest = job['manifest']
-        processed = canvas_processor.CanvasProcess(
-            canvas_obj=canvas, manifest_id=manifest)
-        queue.delete_message(result)
-
-
+    while 1:
+        results = queue.get_messages(wait_time_seconds=20, num_messages=5)
+        for result in results:
+            job = json.loads(result.get_body())
+            canvas = job['canvas']
+            manifest = job['manifest']
+            processed = canvas_processor.CanvasProcess(
+                canvas_obj=canvas, manifest_id=manifest)
+            queue.delete_message(result)
 
 
 if __name__ == '__main__':
