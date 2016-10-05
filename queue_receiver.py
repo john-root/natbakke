afc353a050e3cb2a5de12c934dae30c77cf34aa1
Matt McGrattan <matt.mcgrattan@digirati.com>
2016-10-05 12:20:04 +0100

    Made explicit that it's a canvas queue that's being processed.

--- queue_receiver.py
+++ queue_receiver.py
@@ -5,16 +5,16 @@ import json
 
 def main():
     conn = boto.sqs.connect_to_region("us-west-2")
-    queue = conn.get_queue('ocr1')
+    canvas_queue = conn.get_queue('ocr1')
     while 1:
-        results = queue.get_messages(wait_time_seconds=20, num_messages=5)
-        for result in results:
+        canvas_results = canvas_queue.get_messages(wait_time_seconds=20, num_messages=5)
+        for canvas_result in canvas_results:
             job = json.loads(result.get_body())
             canvas = job['canvas']
             manifest = job['manifest']
             processed = canvas_processor.CanvasProcess(
                 canvas_obj=canvas, manifest_id=manifest)
-            queue.delete_message(result)
+            canvas_queue.delete_message(canvas_result)
 
 
 if __name__ == '__main__':
