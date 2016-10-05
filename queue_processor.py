7d57d18afb769fd7d709ad42fa2f95c959ece128
Matt McGrattan <matt.mcgrattan@digirati.com>
2016-10-05 11:34:07 +0100

    first commit of queue pushing test

---
+++ queue_processor.py
@@ -0,0 +1,19 @@
+import boto.sqs
+import canvas_processor
+import json
+
+
+def main():
+    conn = boto.sqs.connect_to_region("us-west-2")
+    queue = conn.get_queue('ocr1')
+    item = canvas_processor.Manifest(
+        uri='https://tomcrane.github.io/scratch/manifests/ida/m1011-santa-fe-1910-30.json')
+    for canvas in item.canvases:
+        canvas_json = json.dumps(canvas)
+        print canvas_json
+
+
+
+if __name__ == '__main__':
+    main()
+
