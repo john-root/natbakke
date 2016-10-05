623c464addb16a6a48914adeba16d7f8bd24d46e
Matt McGrattan <matt.mcgrattan@digirati.com>
2016-10-05 12:09:20 +0100

    moved the enqueuing to a function.

--- queue_processor.py
+++ queue_processor.py
@@ -3,20 +3,21 @@ import canvas_processor
 import json
 from boto.sqs.message import RawMessage
 
-
-def main():
-    conn = boto.sqs.connect_to_region("us-west-2")
-    queue = conn.get_queue('ocr1')
-    item = canvas_processor.Manifest(
-        uri='https://tomcrane.github.io/scratch/manifests/ida/m1011-santa-fe-1910-30.json')
+def enqueue(queue, manifest_uri):
+    item = canvas_processor.Manifest(manifest_uri)
     for canvas in item.canvases:
         msg = {}
         msg['manifest'] = item.requested.uri
         msg['canvas'] = canvas
         m = RawMessage()
         m.set_body(json.dumps(msg, indent=4))
-        status = queue.write(m)
-        print 'Queued: %s' % canvas['@id']
+        queue.write(m)
+
+
+def main():
+    conn = boto.sqs.connect_to_region("us-west-2")
+    queue = conn.get_queue('ocr1')
+    enqueue(queue, manifest_uri='https://tomcrane.github.io/scratch/manifests/ida/m1011-santa-fe-1910-30.json')
 
 
 
