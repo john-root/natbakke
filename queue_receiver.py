6bbb04f6069de392db400ac8ffc64c2ecb8a25cf
Matt McGrattan <matt.mcgrattan@digirati.com>
2016-10-05 12:26:40 +0100

    PEP8 and changed th result name

--- queue_receiver.py
+++ queue_receiver.py
@@ -7,9 +7,10 @@ def main():
     conn = boto.sqs.connect_to_region("us-west-2")
     canvas_queue = conn.get_queue('ocr1')
     while 1:
-        canvas_results = canvas_queue.get_messages(wait_time_seconds=20, num_messages=5)
+        canvas_results = canvas_queue.get_messages(wait_time_seconds=20,
+                                                   num_messages=5)
         for canvas_result in canvas_results:
-            job = json.loads(result.get_body())
+            job = json.loads(canvas_result.get_body())
             canvas = job['canvas']
             manifest = job['manifest']
             processed = canvas_processor.CanvasProcess(
