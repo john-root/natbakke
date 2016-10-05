e0e4e242132b9baed7569aecbc305c8e00cce2c8
Matt McGrattan <matt.mcgrattan@digirati.com>
2016-10-05 13:02:14 +0100

    Updated the queue name

--- manifest_enqueue.py
+++ manifest_enqueue.py
@@ -11,7 +11,7 @@ def main():
     msg['manifest'] = 'https://tomcrane.github.io/scratch/manifests/ida/m1011-santa-fe-1910-30.json'
     m = RawMessage()
     m.set_body(json.dumps(msg, indent=4))
-    queue.write(m)
+    manifest_queue.write(m)
 
 
 if __name__ == '__main__':
