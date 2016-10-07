d54a4c37d542fb0e172b2bcf5f3846f91b083219
Matt McGrattan <matt.mcgrattan@digirati.com>
2016-10-07 12:17:17 +0100

    First commit of anno handler

---
+++ anno_handler.py
@@ -0,0 +1,17 @@
+from pyld import jsonld
+
+'''
+Serialise and context handling for annos.
+'''
+
+def anno_create():
+    pass
+
+
+def anno_expand():
+    pass
+
+
+def anno_compact():
+    pass
+
