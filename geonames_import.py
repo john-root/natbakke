85865dfef26a86812b7247b36063982f2d658f8b
Matt McGrattan <matt.mcgrattan@digirati.com>
2016-10-10 12:50:52 +0100

    Quick and dirty file to dump out Geonames data as JSON, to be revised to turn this into a gazetteer for Spacy.io

--- geonames_import.py
+++ geonames_import.py
@@ -1,6 +1,8 @@
+# -*- coding: utf-8 -*-
 import csv
 import sys
 csv.field_size_limit(sys.maxsize)
+import ujson
 
 
 foo = open('US.txt')
@@ -8,7 +10,10 @@ foo = open('US.txt')
 fields = ['geonameid', 'name', 'asciiname', 'alternatenames', 'latitude', 'longitude', 'feature class', 'feature code', 'country code', 'cc2',
           'admin1 code', 'admin2 code', 'admin3 code', 'admin4 code', 'population', 'elevation', 'dem', 'timezone', 'modification date']
 
-reader = csv.DictReader(foo, fieldnames=fields, delimiter='\t') #dialect='tab')
+reader = csv.DictReader(foo, fieldnames=fields, delimiter='\t')
 new_mexico = [x for x in reader if x['admin1 code'] == 'NM']
 
-print new_mexico
+# alternates = [y for y in new_mexico if y['alternatenames'] != '']
+
+with open('new_mexico.json', 'w') as export:
+	ujson.dump(new_mexico, export, sort_keys=True, indent=4)
