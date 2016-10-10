47c34c465d414a2f796cd4c3f7a641294746f112
Matt McGrattan <matt.mcgrattan@digirati.com>
2016-10-10 12:55:30 +0100

    PEP8 changes and comments.

--- geonames_import.py
+++ geonames_import.py
@@ -1,19 +1,30 @@
 # -*- coding: utf-8 -*-
 import csv
 import sys
-csv.field_size_limit(sys.maxsize)
 import ujson
 
+'''
+Grab the data from the US export from Geonames,
+filter out just the New Mexico data, and dump
+to an JSON file.
+'''
 
-foo = open('US.txt')
+# need this because of some huge fields,
+# especially alternatenames, which can
+# include Unicode, e.g. cyrillic and
+# arabic transliterations.
+csv.field_size_limit(sys.maxsize)
 
-fields = ['geonameid', 'name', 'asciiname', 'alternatenames', 'latitude', 'longitude', 'feature class', 'feature code', 'country code', 'cc2',
-          'admin1 code', 'admin2 code', 'admin3 code', 'admin4 code', 'population', 'elevation', 'dem', 'timezone', 'modification date']
+text_file = open('US.txt')
 
-reader = csv.DictReader(foo, fieldnames=fields, delimiter='\t')
+# field list, as no header row in tab delimited txt file.
+fields = ['geonameid', 'name', 'asciiname', 'alternatenames', 'latitude',
+          'longitude', 'feature class', 'feature code', 'country code', 'cc2',
+          'admin1 code', 'admin2 code', 'admin3 code', 'admin4 code',
+          'population', 'elevation', 'dem', 'timezone', 'modification date']
+
+reader = csv.DictReader(text_file, fieldnames=fields, delimiter='\t')
 new_mexico = [x for x in reader if x['admin1 code'] == 'NM']
 
-# alternates = [y for y in new_mexico if y['alternatenames'] != '']
-
 with open('new_mexico.json', 'w') as export:
-	ujson.dump(new_mexico, export, sort_keys=True, indent=4)
+    ujson.dump(new_mexico, export, sort_keys=True, indent=4)
