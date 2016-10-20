bbcb8bd9bce2acbe3d7cedeb4dc56fab173f543a
Matt McGrattan <matt.mcgrattan@digirati.com>
2016-10-20 14:30:37 +0200

    Added the initiation and passing of spacy to the canvas processor

--- queue_receiver.py
+++ queue_receiver.py
@@ -1,11 +1,13 @@
 import canvas_processor
 import boto.sqs
 import json
+import spacy.en
 
 
 def main():
     conn = boto.sqs.connect_to_region("us-west-2")
     canvas_queue = conn.get_queue('ocr1')
+    parser = spacy.en.English()
     while 1:
         canvas_results = canvas_queue.get_messages(wait_time_seconds=20,
                                                    num_messages=5)
@@ -13,7 +15,7 @@ def main():
             job = json.loads(canvas_result.get_body())
             canvas = job['canvas']
             manifest = job['manifest']
-            processed = canvas_processor.CanvasProcess(
+            processed = canvas_processor.CanvasProcess(entity_parser=parser,
                 canvas_obj=canvas, manifest_id=manifest, push=True)
             canvas_queue.delete_message(canvas_result)
 
