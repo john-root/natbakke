e94e9f784329d83e0a0b052c3663aeefd113e015
Matt McGrattan <matt.mcgrattan@digirati.com>
2016-10-21 11:46:19 +0200

    Test of manual add of individual entity

--- lod_test.py
+++ lod_test.py
@@ -1,10 +1,21 @@
 from spacy_basics import initialise_spacy
 import ftfy
+from spacy.attrs import ORTH, LOWER
 
 
 def main():
-    example = ftfy.fix_text((open('./data/4d1335b1a2e93cbda27c54638419d073.txt').read().decode('utf-8')))
+    example = ftfy.fix_text(
+        (open('./data/4d1335b1a2e93cbda27c54638419d073.txt').read().decode('utf-8')))
     parser = initialise_spacy('new_mexico.json', geonames=True)
+    parser.matcher.add(
+        "Toadlena",
+        "GPE",
+        [
+            [
+                {ORTH: 'Toadlena',
+                 LOWER: 'toadlena'}
+            ]
+        ])
     parser2 = initialise_spacy()
     print example
     before = parser2(unicode(example))
@@ -15,7 +26,6 @@ def main():
     print("After")
     for ent in after.ents:
         print(ent.text, ent.label_, [w.tag_ for w in ent])
-
 
 
 if __name__ == '__main__':
