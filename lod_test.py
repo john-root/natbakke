344e1690b29aed56fe5ca4612c24eabf1fa46a04
Matt McGrattan <matt.mcgrattan@digirati.com>
2016-10-21 11:33:55 +0200

    More unicode

--- lod_test.py
+++ lod_test.py
@@ -1,18 +1,20 @@
 from spacy_basics import initialise_spacy
+import ftfy
 
 
 def main():
-    example = open('./data/e4488a6cb20ed47e2050daf5e54a9831.txt').read()
+    example = ftfy.fix_text(open('./data/e4488a6cb20ed47e2050daf5e54a9831.txt').read())
     parser = initialise_spacy('new_mexico.json', geonames=True)
     parser2 = initialise_spacy()
-    before = parser(unicode(example))
-    after = parser2(unicode(example))
-    print("Before")
-    for ent in before.ents:
-        print(ent.text, ent.label_, [w.tag_ for w in ent])
-    print("After")
-    for ent in after.ents:
-        print(ent.text, ent.label_, [w.tag_ for w in ent])
+    print example
+    # before = parser(unicode(example))
+    # after = parser2(unicode(example))
+    # print("Before")
+    # for ent in before.ents:
+    #     print(ent.text, ent.label_, [w.tag_ for w in ent])
+    # print("After")
+    # for ent in after.ents:
+    #     print(ent.text, ent.label_, [w.tag_ for w in ent])
 
 
 
