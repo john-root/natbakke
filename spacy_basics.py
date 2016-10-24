9d2585ea35e6566bacb8f12b3393af0435e9b1da
Matt McGrattan <matt.mcgrattan@digirati.com>
2016-10-24 18:00:09 +0100

    Changed dict wrapper to make it a list of dicts not just one per token

--- spacy_basics.py
+++ spacy_basics.py
@@ -53,7 +53,7 @@ def initialise_spacy(matcher_json=None, geonames=False):
             name_tokens = item['asciiname'].split()
             word_list = []
             for token in name_tokens:
-                word_dict = [{LOWER: token.lower()}, {ORTH: token}]
+                word_dict = {LOWER: token.lower()}, {ORTH: token}
                 word_list.append(word_dict)
             try:
                 z = featurecode_dict[item['feature code']]
@@ -74,6 +74,6 @@ def initialise_spacy(matcher_json=None, geonames=False):
                 z,   # Entity type: should be one of the types in the NER data
                 # Arbitrary attributes. Currently unused.
                 {"geonameid": geonameid},
-                word_list
+                [word_list]
             )
     return parser
