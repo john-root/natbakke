d259f780bc417f29dd842ee2624bca546ae78748
Matt McGrattan <matt.mcgrattan@digirati.com>
2016-11-14 20:50:09 +0000

    Quick add of other tribe names from list on web.

--- tribes_import.py
+++ tribes_import.py
@@ -2,7 +2,7 @@ import csv
 import spacy.en
 import spacy.matcher
 from spacy.attrs import ORTH, LOWER
-import json
+# import json
 
 def initialise_tribes(csv_file):
     '''
@@ -11,9 +11,11 @@ def initialise_tribes(csv_file):
     reader = csv.DictReader(open(csv_file))
     parser = spacy.en.English()
     matcher = spacy.matcher.Matcher(parser.vocab)
+    names = []
     for row in reader:
         # master name for the tribe
         name = row['Tribe'].strip()
+        names.append(name)
         '''
         Add the various alternative forms.
         '''
@@ -42,6 +44,7 @@ def initialise_tribes(csv_file):
         add_pat = [''.join(name.split()).lower(), word_list, {"label": name}]
         # print json.dumps(add_pat, indent=4)
         for alt_form in alt_forms:
+            names.append(alt_form)
             word_list = []
             for token in alt_form.split():
                 word_dict = {LOWER: token.strip().lower(), ORTH: token.strip()}
@@ -58,6 +61,29 @@ def initialise_tribes(csv_file):
             # Arbitrary attributes (optional)
             {"ent_type": "TRIBE",
              "loc_id": row['Source or LOC Linked Data Identifier'].strip()},
+            if_exists='update'
+        )
+    txt = open('tribe_list2.txt').read()
+    new_tribes = []
+    for line in txt.split('\n'):
+        additional_tribe = line.split('\t')[0]
+        if additional_tribe not in names:
+            new_tribes.append(additional_tribe)
+    for new_tribe in new_tribes:
+        word_list = []
+        for token in new_tribe.split():
+            word_dict = {LOWER: token.strip().lower(), ORTH: token.strip()}
+            word_list.append(word_dict)
+        matcher.add_pattern(
+            ''.join(new_tribe.split()).lower(),
+            word_list,
+            label=new_tribe)
+        matcher.add_entity(
+            # Entity ID -- Helps you act on the match.
+            ''.join(new_tribe.split()).lower(),
+            # Arbitrary attributes (optional)
+            {"ent_type": "TRIBE",
+             "loc_id": ""},
             if_exists='update'
         )
     return matcher, parser
